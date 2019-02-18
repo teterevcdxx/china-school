1d81fea04aff9fbc372810cf08d4d0836e0bf0e4
unknown <dwemroo@gmail.com>
2019-02-18 16:52:34 +0200

    upd.2.2.2

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,10 +8,16 @@ import Container from "../components/container"
 import Newsbox from "../components/newsbox"
 import Media from "../components/media"
 import Sidemedia from "../components/sideMedia"
+import { Helmet } from "react-helmet"
 
 export default () => (
   <Layout>
     <Viewpage>
+    <Helmet>
+          <meta charSet="utf-8" />
+          <title>China-school | Home</title>
+          <link href="https://fonts.googleapis.com/css?family=Old+Standard+TT:400,700" rel="stylesheet"></link>
+    </Helmet>
     <Header><Sidebar/></Header>
     <Container>
       <Newsbox/>
--- src/pages/location.js
+++ src/pages/location.js
@@ -7,10 +7,16 @@ import Sidebar from "../components/sidebar"
 import Container from "../components/container"
 import Media from "../components/media"
 import Sidemedia from "../components/sideMedia"
+import { Helmet } from "react-helmet"
 
 export default () => (
     <Layout>
       <Viewpage>
+      <Helmet>
+          <meta charSet="utf-8" />
+          <title>China-school | Location</title>
+          <link href="https://fonts.googleapis.com/css?family=Old+Standard+TT:400,700" rel="stylesheet"></link>
+      </Helmet>
       <Header><Sidebar/></Header>
       <Container>
         <Location/>
